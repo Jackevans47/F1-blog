4f657d69a31c583a7748c39effa4152b56a17da9
jackevans47 <jack_evans47@outlook.com>
2024-06-27 10:19:38 +0100

    initial commit

---
+++ .env.py
@@ -0,0 +1,5 @@
+import os
+
+  os.environ['SECRET_KEY'] = '2(22e-b!y42_dl^x^2$h_c@9h)+=7(=ef6j47%=72)lbf4z16g'
+  os.environ['DATABASE_URL'] = 'postgresql://project-4_owner:95hOgZyKWwYI@ep-aged-breeze-a21juwel.eu-central-1.aws.neon.tech/project-4?sslmode=require'
+  os.environ['DEBUG'] = 'True'
