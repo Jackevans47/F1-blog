0f11de7dd34cbbe9df7cd95dc43182357c357c12
jackevans47 <jack_evans47@outlook.com>
2024-07-01 18:16:38 +0100

    edit settings and url

--- .env.py
+++ .env.py
@@ -1,5 +1,5 @@
 import os
 
   os.environ['SECRET_KEY'] = '2(22e-b!y42_dl^x^2$h_c@9h)+=7(=ef6j47%=72)lbf4z16g'
-  os.environ['DATABASE_URL'] = 'postgresql://project-4_owner:95hOgZyKWwYI@ep-aged-breeze-a21juwel.eu-central-1.aws.neon.tech/project-4?sslmode=require'
+  os.environ['DATABASE_URL'] = 'postgres://neondb_owner:1kzSiADT8OIF@ep-broad-voice-a234j65l.eu-central-1.aws.neon.tech/neondb?sslmode=require'
   os.environ['DEBUG'] = 'True'
--- codestar/urls.py
+++ codestar/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from blog.views import my_blog
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("blog/", my_blog, name="blog"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
